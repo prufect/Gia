5fe071303ea524f9d2523daa8ba538287f60f6c8
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-03 12:40:07 -0700

    Refactor Accounts Table

--- Gia/HomeController.swift
+++ Gia/HomeController.swift
@@ -11,11 +11,19 @@ import UIKit
 final class HomeController: UIViewController {
     
     private var emptyListController: EmptyListController? = nil
-    private var accountTableController: AccountsTableController? = nil
     
-    private var accountsData: [AccountsData] = [] {
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.delegate = self
+        tv.dataSource = self
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "AccountCell2")
+        return tv
+    }()
+    
+    private var accounts: [AccountsData] = [] {
         didSet {
-            accountTableController?.reload()
+            tableView.reloadData()
             setupView()
         }
     }
@@ -25,30 +33,29 @@ final class HomeController: UIViewController {
         
         setupNav()
         setupView()
+        setupTableView()
     }
     
     @objc func onAddTapped() {
         let addAccountController = UINavigationController(rootViewController:
             AddAccountController(onAccountTap: { [weak self] accountsData in
-                self?.accountsData.append(accountsData)
+                self?.accounts.append(accountsData)
             }))
         
         present(addAccountController, animated: true, completion: nil)
     }
     
     private func setupView() {
-        if accountsData.isEmpty {
-            if accountTableController != nil {
-                accountTableController?.remove()
-                accountTableController = nil
-            }
-            add(EmptyListController())
+        if accounts.isEmpty {
+            tableView.isHidden = true
+            emptyListController = EmptyListController()
+            add(emptyListController!)
         } else {
+            tableView.isHidden = false
             if emptyListController != nil {
                 emptyListController?.remove()
                 emptyListController = nil
             }
-            add(AccountsTableController(with: accountsData))
         }
     }
     
@@ -56,5 +63,27 @@ final class HomeController: UIViewController {
         title = "Accounts"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAddTapped))
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accounts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell2", for: indexPath)
+        cell.textLabel?.text = accounts[indexPath.row].customerName
+        return cell
     }
 }
