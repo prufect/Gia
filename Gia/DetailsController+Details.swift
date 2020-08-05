31f9fb301738970e9712023bee0ab33f0cd01990
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-05 09:38:30 -0700

    Add Dollar Symbols

--- Gia/DetailsController+Details.swift
+++ Gia/DetailsController+Details.swift
@@ -21,20 +21,20 @@ extension DetailsController {
             .init(name: "Parent Name", content: account.parentCustomerName),
             .init(name: "Parent Number", content: account.parentCustomerNumber),
             .init(name: "DB Rating", content: "\(accountDetails[0].generalInfo.dbRating)"),
-            .init(name: "Last Paid Ammount", content: "\(accountDetails[0].generalInfo.lastPaidAmount)"),
+            .init(name: "Last Paid Ammount", content: "$\(accountDetails[0].generalInfo.lastPaidAmount).00"),
             .init(name: "Last Paid Date", content: accountDetails[0].generalInfo.lastpadiDate),
             .init(name: "Last Credit Review", content: accountDetails[0].generalInfo.lastCreditReview),
             .init(name: "Credit Hold Flag", content: accountDetails[0].generalInfo.creditHoldFlag ? "True" : "False"),
-            .init(name: "Credit Limit", content: "\(accountDetails[0].generalInfo.creditLimit)"),
+            .init(name: "Credit Limit", content: "$\(accountDetails[0].generalInfo.creditLimit).00"),
             .init(name: "Payment Term", content: accountDetails[0].generalInfo.paymentTerm),
         ]
         
         disputesData = [
             .init(name: "Dispute Number", content: disputes[0].disputeNumber),
             .init(name: "Transaction Date", content: disputes[0].transactionDate),
-            .init(name: "Transaction Amount", content: "\(disputes[0].transactionAmount)"),
-            .init(name: "Transaction Due Amount", content: "\(disputes[0].transactionDueAmount)"),
-            .init(name: "Approved Amount", content: "\(disputes[0].approvedAmount)"),
+            .init(name: "Transaction Amount", content: "$\(disputes[0].transactionAmount).00"),
+            .init(name: "Transaction Due Amount", content: "$\(disputes[0].transactionDueAmount).00"),
+            .init(name: "Approved Amount", content: "$\(disputes[0].approvedAmount).00"),
         ]
         
         invoicesData = [
@@ -42,14 +42,14 @@ extension DetailsController {
             .init(name: "Transaction Number", content: invoices[0].transactionNumber),
             .init(name: "Transaction Date", content: invoices[0].transactionDate),
             .init(name: "Transaction Due Date", content: invoices[0].transactionDueDate),
-            .init(name: "Transaction Amount", content: "\(invoices[0].transactionAmount)"),
-            .init(name: "Transaction Due Amount", content: "\(invoices[0].transactionDueAmount)"),
+            .init(name: "Transaction Amount", content: "$\(invoices[0].transactionAmount).00"),
+            .init(name: "Transaction Due Amount", content: "$\(invoices[0].transactionDueAmount).00"),
         ]
         
         paymentsData = [
             .init(name: "Payment Number", content: payments[0].paymentNumber),
             .init(name: "Payment Date", content: payments[0].paymentDate),
-            .init(name: "Payment Amount", content: "\(payments[0].paymentAmount)")
+            .init(name: "Payment Amount", content: "$\(payments[0].paymentAmount).00")
         ]
     }
 }
